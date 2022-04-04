e7c3b6dd0959f38d1a7a879adff2599b3889c5d9
Timon Heinis <timon@embrio.tech>
2022-04-04 17:29:18 +0200

    fix: include tsx, jsx file into lint-staged

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,6 @@ import './App.less'
 import { Button } from 'antd'
 
 function App() {
-  console.log('hello word')
-
   return (
     <div className='p-4'>
       hello world! <Button type='primary'>Button</Button>
