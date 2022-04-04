6e98bbe982926372e368662e5d64cf01ce26f348
Timon Heinis <timon@embrio.tech>
2022-04-04 15:42:17 +0200

    setup react app, tailwind, antd

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.less'
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className='p-4'>
+      hello world!{' '}
+      <Button type='primary'>
+        Button
+      </Button>
+    </div>
+  )
+}
+
+export default App
