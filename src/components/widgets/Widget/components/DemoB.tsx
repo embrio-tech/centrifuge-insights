9fd771ecf8522f5168f1b52251cb961bc647ce7e
Timon Heinis <timon@embrio.tech>
2022-05-31 13:52:58 +0200

    feat: use figure layout for demo widgets

--- src/components/widgets/Widget/components/DemoB.tsx
+++ src/components/widgets/Widget/components/DemoB.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { FigureLayout } from '../layouts'
 // import './DemoB.less'
 
 interface DemoBProps {
-  className?: string;
+  className?: string
 }
 
 const DemoB: React.FC<DemoBProps> = (props) => {
   const { className } = props
 
-  return <div className={className}>DemoB</div>
+  return <FigureLayout className={className} value={'23.45'} unit='%' name='DemoB' color={'#2762ff'} />
 }
 
 export { DemoB }
--- src/components/widgets/Widget/components/DemoC.tsx
+++ src/components/widgets/Widget/components/DemoC.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { FigureLayout } from '../layouts'
 // import './DemoC.less'
 
 interface DemoCProps {
-  className?: string;
+  className?: string
 }
 
 const DemoC: React.FC<DemoCProps> = (props) => {
   const { className } = props
 
-  return <div className={className}>DemoC</div>
+  return <FigureLayout className={className} value={'98.7M'} unit='DAI' name='DemoC' loading />
 }
 
 export { DemoC }
