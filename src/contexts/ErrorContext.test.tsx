26914ebd8dbba5918205653b0d5a66b2d6d0e6b5
Timon Heinis <timon@embrio.tech>
2022-06-07 17:21:43 +0200

    feat: handle errors with global error context

---
+++ src/contexts/ErrorContext.test.tsx
@@ -0,0 +1,7 @@
+describe('ErrorContext should', () => {
+  test.skip('have tests!', () => {
+    // TODO: write tests
+  })
+})
+
+export default null
---
+++ src/contexts/ErrorContext.tsx
@@ -0,0 +1,44 @@
+import { message } from 'antd'
+import React, { createContext, useState, useMemo, useContext, useEffect } from 'react'
+import { AppError } from '../types'
+
+interface ErrorContextInterface {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: AppError | undefined
+  setError: React.Dispatch<React.SetStateAction<AppError | undefined>>
+}
+
+const ErrorContext = createContext<ErrorContextInterface | undefined>(undefined)
+
+const ErrorContextProvider: React.FC = (props) => {
+  const { children } = props
+  const [error, setError] = useState<AppError | undefined>(undefined)
+
+  const value = useMemo<ErrorContextInterface>(
+    () => ({
+      error,
+      setError,
+    }),
+    [error, setError]
+  )
+
+  useEffect(() => {
+    // TODO: improve error messaging for different errors (e.g. ApolloError)
+    if (error) {
+      message.error(error.message, 10)
+      console.error(error)
+    }
+  }, [error])
+
+  return <ErrorContext.Provider value={value}>{children}</ErrorContext.Provider>
+}
+
+const useError = (): ErrorContextInterface => {
+  const context = useContext(ErrorContext)
+  if (!context) {
+    throw new Error('useError must be inside a Provider with a value')
+  }
+  return context
+}
+
+export { ErrorContextProvider, useError }
