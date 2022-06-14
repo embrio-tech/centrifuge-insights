8c13801c712f710e45cad7b0d7f6ad83f204ce5f
Timon Heinis <timon@embrio.tech>
2022-06-14 12:39:55 +0200

    feat: handle paths including IPFS protocoll (ipfs://...)

--- src/clients/ipfs.ts
+++ src/clients/ipfs.ts
@@ -1,5 +1,10 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export const ipfsClient = axios.create({
   baseURL: 'https://ipfs.io/ipfs/',
+})
+
+ipfsClient.interceptors.request.use((config: AxiosRequestConfig) => {
+  const { url } = config
+  return { ...config, url: url?.replace('ipfs://ipfs/', '') }
 })
