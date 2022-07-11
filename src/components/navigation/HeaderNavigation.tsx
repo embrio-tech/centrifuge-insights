987c1fe1d20530cb0f4b974d470bb2c0e0f4778d
Timon Heinis <timon@embrio.tech>
2022-07-11 11:22:48 +0200

    fix: avoid wrapping of header title

--- src/components/navigation/HeaderNavigation.tsx
+++ src/components/navigation/HeaderNavigation.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
-// import './HeaderNavigation.less'
+import './HeaderNavigation.less'
 import logo from '../../svg/centrifuge-marquee-light.svg'
 import { Link } from 'react-router-dom'
 import { Button } from 'antd'
 
 const HeaderNavigation: React.FC = () => {
   return (
-    <div className='flex justify-between h-full'>
-      <div className='py-4 pr-2'>
+    <div className='header-navigation'>
+      <div className='logo'>
         <Link to='/'>
           <img src={logo} alt='logo' className='h-full' />
         </Link>
       </div>
-      <div className='grow text-center'>
-        <h1 className='text-xl sm:text-2xl text-white m-0' style={{ lineHeight: 'inherit' }}>
-          Centrifuge Insights
-        </h1>
-      </div>
-      <div className='hidden sm:block'>
+
+      <h1
+        className='title'
+        style={{ lineHeight: 'inherit' }}
+      >
+        Centrifuge Insights
+      </h1>
+
+      <div className='cta'>
         <Button type='primary' shape='round' href='https://centrifuge.io/' target='_blank'>
           Learn more!
         </Button>
