ca2847ff4dbe3e9a86c5c298e1190239dc4bde9a
Timon Heinis <timon@embrio.tech>
2022-08-16 15:36:20 +0200

    feat: add generic decimal handler

--- src/util/dsmath.ts
+++ src/util/dsmath.ts
@@ -17,3 +17,12 @@ export const wad = (value: string | null): number => Number(value) * 10 ** -18
  * @returns {number} - numerical value
  */
 export const ray = (value: string | null): number => Number(value) * 10 ** -27
+
+/**
+ * converts a number string with a fixed amount of decimals into a number
+ *
+ * @prop {string} value - number string
+ * @prop {number} decimals - amount of decimal digits (default=18)
+ * @returns {number} - numerical value
+ */
+export const decimal = (value: string | null, decimals = 18): number => Number(value) * 10 ** -decimals
