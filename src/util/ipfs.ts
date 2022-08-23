353e2545b4fefa6b11132166f27dfa0296b12d7b
Timon Heinis <timon@embrio.tech>
2022-08-23 16:07:18 +0200

    fix: adapt ipfs file interface to current metadata model

--- src/util/ipfs.ts
+++ src/util/ipfs.ts
@@ -2,5 +2,5 @@ import type { IpfsFile } from '../types'
 
 export const getIpfsHash = (file?: string | IpfsFile): string | undefined => {
   if (typeof file === 'string') return file.replace('ipfs://ipfs/', '')
-  return file?.ipfsHash
+  return file?.uri
 }
