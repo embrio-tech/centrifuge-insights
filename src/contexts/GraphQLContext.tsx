fba4e1d5f8eee64be5cec579ae465e3d796c5126
Timon Heinis <timon@embrio.tech>
2022-10-04 16:26:19 +0200

    refactor: prepare refetching for reached page size limit

--- src/contexts/GraphQLContext.tsx
+++ src/contexts/GraphQLContext.tsx
@@ -4,6 +4,35 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { useTenant } from './TenantContext'
 
+// // TODO: https://www.apollographql.com/docs/react/pagination/core-api/#merging-paginated-results
+// const cache = new InMemoryCache({
+//   typePolicies: {
+//     Query: {
+//       fields: {
+//         trancheSnapshots: {
+//           // Don't cache separate results based on
+//           // any of this field's arguments.
+//           keyArgs: false,
+
+//           // Concatenate the incoming list items with
+//           // the existing list items.
+//           merge(
+//             existing: Nodes<TrancheSnapshot> = { totalCount: 0, nodes: [] },
+//             incoming: Nodes<TrancheSnapshot>
+//           ): Nodes<TrancheSnapshot> {
+//             return {
+//               totalCount: incoming.totalCount,
+//               nodes:
+//                 incoming.nodes.length !== incoming.totalCount
+//                  ? [...existing.nodes, ...incoming.nodes] : existing.nodes,
+//             }
+//           },
+//         },
+//       },
+//     },
+//   },
+// })
+
 export const GraphQLContextProvider: React.FC<PropsWithChildren> = (props) => {
   const { children } = props
   const {
