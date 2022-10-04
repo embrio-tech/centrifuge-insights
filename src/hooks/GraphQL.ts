ddf2d169f7a8e4fe1fc45ea98617c5e05e24429f
Timon Heinis <timon@embrio.tech>
2022-10-04 17:28:55 +0200

    fix: dont overwrite useQuery default options

--- src/hooks/GraphQL.ts
+++ src/hooks/GraphQL.ts
@@ -12,7 +12,7 @@ import { useError } from '../contexts/ErrorContext'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useGraphQL = <Data = any, Variables = OperationVariables>(
   query: DocumentNode | TypedDocumentNode<Data, Variables>,
-  options: QueryHookOptions<Data, Variables> = {}
+  options?: QueryHookOptions<Data, Variables>
 ): Omit<QueryResult<Data, Variables>, 'error'> => {
   const { error, data, ...rest } = useQuery<Data, Variables>(query, options)
   const { setError } = useError()
@@ -23,7 +23,7 @@ export const useGraphQL = <Data = any, Variables = OperationVariables>(
 
   // raise error if not all documents are fetched with one request (workaround). TODO: find better solution.
   useEffect(() => {
-    const { variables } = options
+    const { variables } = options || {}
     if (data && variables && !('offset' in variables) ) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       Object.values<any>(data)
