eb6beae4dba32367d246497d796af6326b2900d1
Timon Heinis <timon@embrio.tech>
2022-10-04 16:23:30 +0200

    fix: temporary fix bug when reaching trancheSnapshot limit of 100

--- src/components/widgets/Widget/components/InvestmentVolume.tsx
+++ src/components/widgets/Widget/components/InvestmentVolume.tsx
@@ -90,20 +90,22 @@ export const InvestmentVolume: React.FC<InvestmentVolumeProps> = (props) => {
     }
   `
 
-  const variables = useMemo(
-    () => ({
-      poolId: selections.pool?.[0],
-      from: new Date('2022-06-04'),
-      to: new Date(),
-      tranches: selections.tranches?.map((trancheId) => ({ trancheId: { endsWith: trancheId } })),
-    }),
-    [selections]
-  )
+const variables = useMemo(() => {
+  const to = new Date()
+  const days = Math.floor(100 / (selections.tranches?.length || 1))
+  const from = new Date()
+  from.setDate(from.getDate() - days)
+
+  return {
+    poolId: selections.pool?.[0],
+    to,
+    from,
+    tranches: selections.tranches?.map((trancheId) => ({ trancheId: { endsWith: trancheId } })),
+  }
+}, [selections])
 
   const skip = useMemo(
-    () =>
-      Object.values(variables).reduce((variableMissing, variable) => variableMissing || !variable, false) ||
-      !filtersReady,
+    () => Object.values(variables).every((variable) => !variable) || !filtersReady,
     [variables, filtersReady]
   )
 
